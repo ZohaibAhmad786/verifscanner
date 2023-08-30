ee4f3ce0ca2b0dc4aa6ba848cf40674722ccf9d5
Zohaib Ahmad <ahmadzohaib369@gmail.com>
2023-08-30 18:02:25 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+import QRScan from "./src/screens/qrScan";
+import IdentityVerification from "./src/screens/IdentityVerification";
+import DocumentType from "./src/screens/DocumentType";
+import DocumentVerification from './src/screens/DocumentVerification';
+import ValidationError from "./src/screens/ValidationError";
+import SelfieVerification from './src/screens/SelfieVerification';
+import WaitingScreen from './src/screens/WaitingScreen'
+import FinishedScreen from "./src/screens/FinishedScreen";
+
+export {
+    QRScan,
+    IdentityVerification,
+    DocumentType,
+    DocumentVerification,
+    ValidationError,
+    SelfieVerification,
+    WaitingScreen,
+    FinishedScreen
+}
+
